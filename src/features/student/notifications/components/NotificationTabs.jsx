d67534d1d39b41f7f6f91491f50c518a2f92615a
Abdulz11 <abduloyewale@gmail.com>
2026-09-11 22:46:28 +0100

    feat: enhance notification icons to match colors of notifications

--- src/features/student/notifications/components/NotificationTabs.jsx
+++ src/features/student/notifications/components/NotificationTabs.jsx
@@ -1,17 +1,30 @@
 import { Bell } from "lucide-react";
-
+import { Circle, UserRoundCheck, BellDot, Ribbon } from "lucide-react";
+const icons = {
+  info: BellDot,
+  success: UserRoundCheck,
+  muted: Ribbon,
+};
 function NotificationIcon({ tone }) {
   const palette = {
     success: "bg-emerald-50 text-emerald-700",
     info: "bg-emerald-50 text-emerald-700",
     muted: "bg-slate-200/70 text-slate-500",
   };
+  const color = {
+    info: "yellow",
+    muted: "white",
+    success: "green",
+    warning: "red",
+  };
+
+  const Icon = icons[tone];
 
   return (
     <div
       className={`grid h-6 w-6 place-items-center rounded-lg ${palette[tone] ?? palette.info}`}
     >
-      <Bell />
+      <Icon color={color[tone]} />
     </div>
   );
 }
@@ -20,11 +33,20 @@ function NotificationTabs({ group, item }) {
     <>
       <article
         key={`${group.label}-${item.title}`}
-        className={`grid gap-3 rounded-2xl  bg-[#10B98108] border border-slate-200/80 p-4 first:border-t-0 sm:gap-4 md:grid-cols-[26px_minmax(0,1fr)_auto] md:py-5 ${
+        className={`grid gap-3 rounded-2xl  bg-[#10B98108] border border-slate-200/80 p-4 first:border-t-0 sm:gap-4 md:grid-cols-[26px_auto_minmax(0,1fr)_auto] md:py-5 ${
           item.tone === "muted" ? "text-slate-500" : "text-slate-800"
         }`}
       >
-        <NotificationIcon tone={item.tone} />
+        <span>
+          {item.category == "Milestones" ? (
+            <Circle fill='green' color='green' size={10} />
+          ) : (
+            ""
+          )}
+        </span>
+        <button className='border border-slate-300 rounded-2xl px-4 py-0'>
+          <NotificationIcon tone={item.tone} />
+        </button>
 
         <div className='min-w-0'>
           <span
